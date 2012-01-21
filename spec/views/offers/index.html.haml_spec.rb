8c9317eae499872a90460122571f410a778f79ed
Roman <broilerster@gmail.com>
2012-01-22 01:02:38 +0200

    Added specs for offers#index

--- spec/views/offers/index.html.haml_spec.rb
+++ spec/views/offers/index.html.haml_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe "offers/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:thumbnail) { mock(:thumbnail, lowres: 'image1.png') }
+  let (:offer1) { mock(:offer, title: 'Offer 1', payout: 10, thumbnail: thumbnail) }
+  let (:offer2) { mock(:offer, title: 'Offer 2', payout: 20, thumbnail: thumbnail) }
+
+  context "when form has been submitted" do
+    before do
+      assign(:search, mock(successful?: true))
+      assign(:offers, paginated_array([offer1, offer2]))
+    end
+
+    it "displays the offers list if search was successful" do
+      render
+      rendered.should =~ /Offer 1/
+    end
+
+    it "renders _offer partial for each offer" do
+      render
+      view.should render_template(:partial => "_offer", :count => 2)
+    end
+  end
+
+  def paginated_array(array)
+    WillPaginate::Collection.create(1, 20, array.length) do |pager|
+      pager.replace array[pager.offset, pager.per_page].to_a
+    end
+  end
 end
