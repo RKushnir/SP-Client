7fc2720033a7abe8117236e8fd8a24ae21973a6b
Roman <broilerster@gmail.com>
2012-01-21 11:39:30 +0200

    Added root SponsorPay module with configuration settings

---
+++ lib/sponsor_pay.rb
@@ -0,0 +1,27 @@
+module SponsorPay
+  # Secret key you received from SponsorPay
+  mattr_accessor :api_key
+
+  # Path to configuration yaml file
+  mattr_accessor :config_path
+
+  # Address of the API host
+  mattr_accessor :host
+  @@host = "http://api.sponsorpay.com"
+
+  # Relative path for this version of API
+  mattr_accessor :path
+  @@path = "/feed/v1/offers"
+
+  # Format - json or xml
+  mattr_accessor :format
+  @@format = 'json'
+
+  # The SponsorPay application ID for your application
+  mattr_accessor :application_id
+
+  def self.setup
+    yield(self)
+  end
+
+end
