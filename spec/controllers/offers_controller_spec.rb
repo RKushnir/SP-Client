5669e64639073ba8328eeb0f62de1b0bf363af80
Roman <broilerster@gmail.com>
2012-01-22 00:22:29 +0200

    Added offers controller spec

--- spec/controllers/offers_controller_spec.rb
+++ spec/controllers/offers_controller_spec.rb
@@ -7,6 +7,24 @@ describe OffersController do
       get 'index'
       response.should be_success
     end
+
+    it "returns bad request when submitted with invalid parameters" do
+      get 'index', :submit => true, :user_id => ''
+      response.should be_bad_request
+    end
+
+    it "assigns offers" do
+      OfferSearch.any_instance.stub(:successful? => true, :offers => [1, 2, 3])
+      get 'index', :submit => 'submit', :user_id => 'qwerty', :custom => ['asdfgh']
+      subject.should assign_to(:offers).with([1, 2, 3])
+    end
+
+    it "assigns search" do
+      OfferSearch.any_instance.stub(successful?: true, offers: [1, 2, 3])
+      get 'index', :submit => 'submit', :user_id => 'qwerty', :custom => ['asdfgh']
+      subject.should assign_to(:search)
+    end
+
   end
 
 end
