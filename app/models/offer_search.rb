737177bab78891f482055a6be437f76429e6cfb4
Roman <broilerster@gmail.com>
2012-01-21 22:59:31 +0200

    Added proxy methods in OfferSearch to return default values if request failed

--- app/models/offer_search.rb
+++ app/models/offer_search.rb
@@ -1,8 +1,20 @@
 class OfferSearch
-  delegate :successful?, :offers, :message, :count, :pages, :information, :to => :result
+  delegate :successful?, :message, :information, :to => :result
 
   def initialize(params={})
     @request = SponsorPay::Request.new(params)
+  end
+
+  def offers
+    successful? ? result.offers : []
+  end
+
+  def count
+    successful? ? result.count : 0
+  end
+
+  def pages
+    successful? ? result.pages : 0
   end
 
   private
